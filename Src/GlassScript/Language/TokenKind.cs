92d73e95901a1921a9af68658be9ca0ed79332a7
seaboy1234 <seaboy1234@gmail.com>
2015-04-30 09:04:02 -0700

    Implement TokenKind.

--- Src/GlassScript/Language/TokenKind.cs
+++ Src/GlassScript/Language/TokenKind.cs
@@ -8,5 +8,110 @@ namespace GlassScript.Language
 {
     public enum TokenKind
     {
+        EndOfFile,
+        Error,
+
+        #region WhiteSpace
+
+        WhiteSpace,
+        NewLine,
+
+        #endregion WhiteSpace
+
+        #region Comments
+
+        LineComment,
+        BlockComment,
+
+        #endregion Comments
+
+        #region Constants
+
+        IntegerLiteral,
+        StringLiteral,
+        FloatLiteral,
+
+        #endregion Constants
+
+        #region Identifiers
+
+        Identifier,
+        Keyword,
+
+        #endregion Identifiers
+
+        #region Groupings
+
+        LeftBracket, // {
+        RightBracket, // }
+        RightBrace, // ]
+        LeftBrace, // [
+        LeftParenthesis, // (
+        RightParenthesis, // )
+
+        #endregion Groupings
+
+        #region Operators
+
+        GreaterThanOrEqual, // >=
+        GreaterThan, // >
+
+        LessThan, // <
+        LessThanOrEqual, // <=
+
+        PlusEqual, // +=
+        PlusPlus, // ++
+        Plus, // +
+
+        MinusEqual, // -=
+        MinusMinus, // --
+        Minus, // -
+
+        Assignment, // =
+
+        Not, // !
+        NotEqual, // !=
+
+        Mul, // *
+        MulEqual, // *=
+
+        Div, // /
+        DivEqual, // /=
+
+        BooleanAnd, // &&
+        BooleanOr, // ||
+
+        BitwiseAnd, // &
+        BitwiseOr, // |
+
+        BitwiseAndEqual, // &=
+        BitwiseOrEqual, // |=
+
+        ModEqual, // %=
+        Mod, // %
+
+        BitwiseXorEqual, // ^=
+        BitwiseXor, // ^
+
+        DoubleQuestion, // ??
+        Question, // ?
+
+        Equal, // ==
+
+        BitShiftLeft, // <<
+        BitShiftRight, // >>
+
+        #endregion Operators
+
+        #region Punctuation
+
+        Dot,
+        Comma,
+        Semicolon,
+        Colon,
+        Arrow, // ->
+        FatArrow, // =>
+
+        #endregion Punctuation
     }
 }
